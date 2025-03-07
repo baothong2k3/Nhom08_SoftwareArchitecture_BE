8ddd8a4446130b5af7d92b79603ded86b3edee40
MSI\PC BAO THONG <baothong15082003@gmail.com>
2025-03-08 00:33:29 +0700

    entity, service

--- cart-service/src/main/java/com/bookstore/services/CartService.java
+++ cart-service/src/main/java/com/bookstore/services/CartService.java
@@ -1,4 +1,19 @@
 package com.bookstore.services;
 
-public class CartService {
+import com.bookstore.entities.Cart;
+
+import java.util.List;
+
+public interface CartService {
+    public Cart saveCart(Long bookId, Long userId);
+
+    public List<Cart> getCartsByUser(Long userId);
+
+    public Integer getCountCart(Long userId);
+
+    public void updateQuantity(String sy, Long cartId);
+
+    public void deleteBookInCart(Long cartId);
+
+    public void updateQuantityBookInCart(Cart cart);
 }
