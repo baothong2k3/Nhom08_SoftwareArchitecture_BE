9af236f616e1bcc7058271f45209c8c277792440
zit nguyen <lebaobinh20036@gmail.com>
2025-03-15 07:15:36 +0700

    Update CartService

--- cart-service/src/main/java/com/bookstore/controllers/CartController.java
+++ cart-service/src/main/java/com/bookstore/controllers/CartController.java
@@ -1,13 +1,35 @@
 package com.bookstore.controllers;
 
+import com.bookstore.entities.Cart;
 import com.bookstore.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
     @Autowired
     private CartService cartService;
+
+    @PostMapping("/add")
+    public Cart addBookToCart(@RequestParam Long userId, @RequestParam Long bookId, @RequestParam int stockQuantity) {
+        return cartService.saveCart(bookId, userId, stockQuantity);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Cart> getUserCart(@PathVariable Long userId) {
+        return cartService.getCartsByUser(userId);
+    }
+
+    @PutMapping("/update")
+    public void updateCart(@RequestParam String action, @RequestParam Long userId, @RequestParam Long bookId, @RequestParam int stockQuantity) {
+        cartService.updateQuantity(action, userId, bookId, stockQuantity);
+    }
+
+    @DeleteMapping("/remove")
+    public void removeBookFromCart(@RequestParam Long userId, @RequestParam Long bookId) {
+        cartService.deleteBookInCart(userId, bookId);
+    }
 }
--- cart-service/src/main/java/com/bookstore/services/CartService.java
+++ cart-service/src/main/java/com/bookstore/services/CartService.java
@@ -1,19 +1,12 @@
 package com.bookstore.services;
 
 import com.bookstore.entities.Cart;
-
 import java.util.List;
 
 public interface CartService {
-    public Cart saveCart(Long bookId, Long userId);
-
-    public List<Cart> getCartsByUser(Long userId);
-
-    public Integer getCountCart(Long userId);
-
-    public void updateQuantity(String sy, Long cartId);
-
-    public void deleteBookInCart(Long cartId);
-
-    public void updateQuantityBookInCart(Cart cart);
+    Cart saveCart(Long bookId, Long userId, int stockQuantity);
+    List<Cart> getCartsByUser(Long userId);
+    Integer getCountCart(Long userId);
+    void updateQuantity(String action, Long userId, Long bookId, int stockQuantity);
+    void deleteBookInCart(Long userId, Long bookId);
 }
