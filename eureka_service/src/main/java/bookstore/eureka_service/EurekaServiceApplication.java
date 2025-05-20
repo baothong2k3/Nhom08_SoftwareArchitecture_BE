db38f092afbc77ac075beebcafda72337901e872
MSI\PC BAO THONG <baothong15082003@gmail.com>
2025-05-20 18:37:27 +0700

    Add docker + docker compose for eureka_service, api-gateway, book-service

--- eureka_service/src/main/java/bookstore/eureka_service/EurekaServiceApplication.java
+++ eureka_service/src/main/java/bookstore/eureka_service/EurekaServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
-@EnableEurekaServer  // Enable eureka server
+@EnableEurekaServer
 public class EurekaServiceApplication {
 
     public static void main(String[] args) {
